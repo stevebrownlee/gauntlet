1599d45e609a07cadaacfb1d1eefb5bf3a49cd6a
Steve Brownlee <chortlehoort@gmail.com>
2017-03-09 15:17:31 -0600

    Horde converted to use WeakMap and ObjectExtensions This is the first commit in converting Gauntlet to use WeakMaps to store private data in each module so that when/if references are lost, they can be fully garbage collected

--- javascripts/horde.js
+++ javascripts/horde.js
@@ -1,92 +1,101 @@
 "use strict";
 
-var Gauntlet = function ($$gauntlet) {
+var Gauntlet = function (global) {
 
-  $$gauntlet.Horde = (() => {
-    let horde = new Map();
-    let names = new Set();
-    let _internal = Object.create(null);
+  let _private = new WeakMap();
 
+  let _internal = function (object) {
+    if (!_private.has(object))
+        _private.set(object, Object.create(null));
+    return _private.get(object);
+  };
+  
+  let Horde = __.compose(Object.create(null), ObjectExtensions);
 
-    _internal.all = () => {
-      return horde;
-    };
+  Horde.def("init", function () {
+    _internal(this).horde = new Map();
+    _internal(this).names = new Set();
+    return this;
+  });
 
-    _internal.soldier = type => {
-      let soldier = Object.create(horde.get(type));
-      return soldier;
-    };
+  Horde.def("all", function () {
+    return _internal(this).horde;
+  });
 
-    _internal.random = () => {
-      let randomSoldier = null;
+  Horde.def("soldier", function () {
+    let soldier = Object.create(_internal(this).horde.get(type));
+    return soldier;
+  });
 
-      /*
-        The Monster object needs to remain in the horde Map to
-        maintain the prototype chain, but it can't be the basis
-        for an actual soldier instance.
-       */
-      do {
-        randomSoldier = horde.random();
-      } while (randomSoldier.id === "Monster");
+  Horde.def("random", function () {
+    let randomSoldier = null;
 
-      // Give the soldier a random name
-      randomSoldier.name = names.random();
+    /*
+      The Monster object needs to remain in the horde Map to
+      maintain the prototype chain, but it can't be the basis
+      for an actual soldier instance.
+      */
+    do {
+      randomSoldier = _internal(this).horde.random();
+    } while (randomSoldier.id === "Monster");
 
-      return randomSoldier;
-    };
+    // Give the soldier a random name
+    randomSoldier.name = _internal(this).names.random();
 
-    _internal.load = () => {
-      return new Promise((resolve, reject) => {
-        $.ajax({url: "./data/horde.json"}).done(response => {
+    return randomSoldier;
+  });
 
-          /*
-            Load the names array in the JSON file into a Set
-           */
-          response.names.each(name => {
-            names.add(name);
-          });
+  Horde.def("load", function () {
+    return new Promise((resolve, reject) => {
+      $.ajax({url: "./data/horde.json"}).done(response => {
 
-          /*
-            Iterate over the objects in the class key and start
-            setting up the prototype chain, which is simplistic
-            since all specific enemies inherit from Monster at
-            this point.
-           */
-          response.classes.each(monster => {
-            let object_prototype;
-
-            // The base monster will always have Player as its prototype
-            if (monster.prototype === null) {
-              object_prototype = $$gauntlet.Army.Player;
-            } else  {
-              object_prototype = horde.get(monster.prototype);
-            };
-
-            /*
-              Create a new object for the current monster based on the
-              corresponding prototype
-             */
-            let monster_for_map = __.compose(object_prototype, monster, {
-              species: monster["id"]
-            });
-
-            // Add creature to the horde Map
-            horde.set(monster.id, monster_for_map);
-          });
-
-          // Resolve the promise
-          resolve();
-
-        }).fail((xhr, error, msg) => {
-          console.error(msg);
-          reject();
+        /*
+          Load the names array in the JSON file into a Set
+          */
+        response.names.each(name => {
+          _internal(this).names.add(name);
         });
+
+        /*
+          Iterate over the objects in the class key and start
+          setting up the prototype chain, which is simplistic
+          since all specific enemies inherit from Monster at
+          this point.
+          */
+        response.classes.each(monster => {
+          let object_prototype;
+
+          // The base monster will always have Player as its prototype
+          if (monster.prototype === null) {
+            object_prototype = global.Army.Player;
+          } else  {
+            object_prototype = _internal(this).horde.get(monster.prototype);
+          };
+
+          /*
+            Create a new object for the current monster based on the
+            corresponding prototype
+            */
+          let monster_for_map = __.compose(object_prototype, monster, {
+            species: monster["id"]
+          });
+
+          // Add creature to the horde Map
+          _internal(this).horde.set(monster.id, monster_for_map);
+        });
+
+        // Resolve the promise
+        resolve();
+
+      }).fail((xhr, error, msg) => {
+        console.error(msg);
+        reject();
       });
-    };
+    });
+  });
 
-    return _internal;
-  })();
+  global.Horde = Horde.init();
 
-  return $$gauntlet;
+  return global;
 
 }(Gauntlet || {});
